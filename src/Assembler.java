2976749ea8d34aa6220d29a9111d4075bf1ff4ce
Lakshay Sharma <lakshay.sharma10@gmail.com>
2018-10-24 23:03:43 +0530

    First Deadline

--- src/Assembler.java
+++ src/Assembler.java
@@ -2,11 +2,42 @@ import java.io.*;
 import java.util.*; 
 
 class instruction{
-	char label;
+	String opcode;
+	String ascode;
+	String var;
 	
+	public instruction(String n) {
+		
+	}
 }
 
 public class Assembler {
+	
+	static class OpCode{
+		private Map<String, String> opcode;
+		
+		public OpCode() {
+			opcode = new TreeMap<String, String>();
+			opcode.put("CLA","0000");
+			opcode.put("LAC","0001");
+			opcode.put("SAC","0010");
+			opcode.put("ADD","0011");
+			opcode.put("SUB","0100");
+			opcode.put("BRZ","0101");
+			opcode.put("BRN","0110");
+			opcode.put("BRP","0111");
+			opcode.put("INP","1000");
+			opcode.put("DSP","1001");
+			opcode.put("MUL","1010");
+			opcode.put("DIV","1011");
+			opcode.put("STP","1100");
+		}
+		
+		public String getOpCode(String n) {
+			return opcode.get(n);
+		}
+		
+	}
 
 	public static void main(String[] args) throws FileNotFoundException {
 		File f = new File("C:\\Users\\Lakshay\\Desktop\\test.txt");
